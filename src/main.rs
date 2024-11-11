299b12b9a300e22a86ee2e0eca6b3479ac36f06a
Alexandre ABRIOUX <alexandre-abrioux@users.noreply.github.com>
2024-11-11 17:07:57 +0100

    feat: allow custom item config

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use clap::Parser;
 use markdown_table_formatter::format_tables;
 use std::fs;
 use yaml_rust2::yaml::Hash;
-use yaml_rust2::YamlLoader;
+use yaml_rust2::{Yaml, YamlLoader};
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
@@ -70,12 +70,12 @@ fn generate_header(tools: &Hash) -> String {
 }
 
 fn generate_items(tools: &Hash) -> String {
-    let lines_nb = tools.keys().fold(0, |max_tools_length, category| {
-        let category_items = tools[category].as_vec().unwrap();
-        if category_items.len() > max_tools_length {
-            return category_items.len();
+    let lines_nb = tools.keys().fold(0, |max_items_count, category| {
+        let category_items_count = tools[category].as_vec().unwrap().len();
+        if category_items_count > max_items_count {
+            return category_items_count;
         }
-        return max_tools_length;
+        return max_items_count;
     });
 
     let mut items = String::new();
@@ -85,7 +85,7 @@ fn generate_items(tools: &Hash) -> String {
             let category_items = tools[category].as_vec().unwrap();
             if line <= category_items.len() {
                 let item = &category_items[line - 1];
-                items.push_str(generate_img_tag(item.as_str().unwrap()).as_str());
+                items.push_str(generate_img_tag(item).as_str());
             }
             items.push_str("|");
         });
@@ -94,19 +94,85 @@ fn generate_items(tools: &Hash) -> String {
     items
 }
 
-fn generate_img_tag(slug: &str) -> String {
+struct Item {
+    label: String,
+    color: String,
+    icon: Option<String>,
+}
+
+fn extract_string_from_hash(hash: &Hash, key: &str) -> String {
+    let value = Yaml::String(key.to_string());
+    hash.get(&value)
+        .expect(format!("missing {key} for item").as_str())
+        .as_str()
+        .expect(format!("{key} is not a string").as_str())
+        .to_string()
+}
+
+fn generate_img_tag(item: &Yaml) -> String {
+    if !item.is_hash() {
+        return generate_img_tag_from_slug(item.as_str().unwrap());
+    }
+    let item_hash = item.as_hash().unwrap();
+    let icon = item_hash
+        .get(&Yaml::String("icon".to_string()))
+        .map(|s| s.as_str().unwrap().to_string());
+    let mut color_option: Option<String> = None;
+    if icon.is_some() {
+        let icon_from_slug = generate_item_from_slug(icon.unwrap().as_str());
+        color_option = Some(icon_from_slug.color)
+    }
+    if item_hash.get(&Yaml::String("color".to_string())).is_some() {
+        color_option = Some(extract_string_from_hash(item_hash, "color"))
+    }
+    let label = extract_string_from_hash(item_hash, "label");
+    let color = color_option
+        .expect(format!("missing color or icon for item {label}").as_str())
+        .to_string();
+    let item = Item {
+        label,
+        color,
+        icon: item_hash
+            .get(&Yaml::String("icon".to_string()))
+            .map(|s| s.as_str().unwrap().to_string()),
+    };
+    generate_img_tag_from_item(&item)
+}
+
+fn generate_img_tag_from_slug(slug: &str) -> String {
+    generate_img_tag_from_item(&generate_item_from_slug(&slug))
+}
+
+fn generate_item_from_slug(slug: &str) -> Item {
     let icons = &SimpleIcons::global().icons;
     let icon = icons
         .get(slug)
         .expect(format!("Could not find icon for slug {slug}").as_str());
     let title = &icon.title;
     let hex = &icon.hex;
-    let background = if is_relatively_light_icon_hex(&hex) {
+    Item {
+        label: title.to_string(),
+        color: hex.to_string(),
+        icon: Some(slug.to_string()),
+    }
+}
+
+fn generate_img_tag_from_item(item: &Item) -> String {
+    let mut tag = String::new();
+    let label = &item.label;
+    let color = &item.color;
+    let background = if is_relatively_light_icon_hex(&color) {
         "black"
     } else {
         "white"
     };
-    format!("[<img align=\"left\" alt=\"{title}\" src=\"https://img.shields.io/badge/-{title}-{hex}?logo={slug}&logoColor={background}\">](#)")
+    tag.push_str(format!(r#"[<img align="left" alt="{label}" src="https://img.shields.io/badge/-{label}-{color}?logoColor={background}"#).as_str());
+    if item.icon.is_some() {
+        let icon = item.icon.clone().unwrap();
+        tag.push_str(format!("&logo={icon}").as_str());
+    }
+    tag.push_str(r#"">](#)"#);
+    tag
 }
 
 fn update_readme(readme_path: &String, toolbox_markdown: &String) {
@@ -144,11 +210,11 @@ tools:
         let markdown = generate_toolbox(&input.to_string());
         assert_eq!(
             markdown,
-            "| ides                                                                                                                        | languages                                                                                                                      |
+            r###"| ides                                                                                                                        | languages                                                                                                                      |
 | --------------------------------------------------------------------------------------------------------------------------- | ------------------------------------------------------------------------------------------------------------------------------ |
-| [<img align=\"left\" alt=\"JetBrains\" src=\"https://img.shields.io/badge/-JetBrains-000000?logo=jetbrains&logoColor=white\">](#) | [<img align=\"left\" alt=\"JavaScript\" src=\"https://img.shields.io/badge/-JavaScript-F7DF1E?logo=javascript&logoColor=black\">](#) |
-| [<img align=\"left\" alt=\"Neovim\" src=\"https://img.shields.io/badge/-Neovim-57A143?logo=neovim&logoColor=white\">](#)          | [<img align=\"left\" alt=\"Rust\" src=\"https://img.shields.io/badge/-Rust-000000?logo=rust&logoColor=white\">](#)                   |
-"
+| [<img align="left" alt="JetBrains" src="https://img.shields.io/badge/-JetBrains-000000?logo=jetbrains&logoColor=white">](#) | [<img align="left" alt="JavaScript" src="https://img.shields.io/badge/-JavaScript-F7DF1E?logo=javascript&logoColor=black">](#) |
+| [<img align="left" alt="Neovim" src="https://img.shields.io/badge/-Neovim-57A143?logo=neovim&logoColor=white">](#)          | [<img align="left" alt="Rust" src="https://img.shields.io/badge/-Rust-000000?logo=rust&logoColor=white">](#)                   |
+"###
         );
     }
 
@@ -164,11 +230,36 @@ tools:
         let markdown = generate_toolbox(&input.to_string());
         assert_eq!(
             markdown,
-            "| ides                                                                                                               | languages                                                                                                                      |
+            r#"| ides                                                                                                               | languages                                                                                                                      |
 | ------------------------------------------------------------------------------------------------------------------ | ------------------------------------------------------------------------------------------------------------------------------ |
-| [<img align=\"left\" alt=\"Neovim\" src=\"https://img.shields.io/badge/-Neovim-57A143?logo=neovim&logoColor=white\">](#) | [<img align=\"left\" alt=\"JavaScript\" src=\"https://img.shields.io/badge/-JavaScript-F7DF1E?logo=javascript&logoColor=black\">](#) |
-|                                                                                                                    | [<img align=\"left\" alt=\"Rust\" src=\"https://img.shields.io/badge/-Rust-000000?logo=rust&logoColor=white\">](#)                   |
-"
+| [<img align="left" alt="Neovim" src="https://img.shields.io/badge/-Neovim-57A143?logo=neovim&logoColor=white">](#) | [<img align="left" alt="JavaScript" src="https://img.shields.io/badge/-JavaScript-F7DF1E?logo=javascript&logoColor=black">](#) |
+|                                                                                                                    | [<img align="left" alt="Rust" src="https://img.shields.io/badge/-Rust-000000?logo=rust&logoColor=white">](#)                   |
+"#
+        );
+    }
+
+    #[test]
+    fn should_handle_tools_with_custom_config() {
+        let input = "
+tools:
+  ides:
+    - label: VSCode
+      color: 29a9f2
+    - label: RustRover
+      icon: jetbrains
+    - label: RustRover
+      icon: jetbrains
+      color: feab02
+";
+        let markdown = generate_toolbox(&input.to_string());
+        assert_eq!(
+            markdown,
+            r#"| ides                                                                                                                        |
+| --------------------------------------------------------------------------------------------------------------------------- |
+| [<img align="left" alt="VSCode" src="https://img.shields.io/badge/-VSCode-29a9f2?logoColor=white">](#)                      |
+| [<img align="left" alt="RustRover" src="https://img.shields.io/badge/-RustRover-000000?logoColor=white&logo=jetbrains">](#) |
+| [<img align="left" alt="RustRover" src="https://img.shields.io/badge/-RustRover-feab02?logoColor=black&logo=jetbrains">](#) |
+"#
         );
     }
 }
